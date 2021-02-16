424b4bc0d4cf1e865e4cd02e55c68bcd2f0ee2f2
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-02-16 15:07:53 +0100

    setup.py fixed typo

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+    
+setuptools.setup(
+  name = 'caffeination',
+  version = '0.1', 
+  license='MIT',
+  description = 'Caffeination makes sure your computes does not go to sleep. It is a sleep preventer.',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  author = 'Antonio Raffaele Iannaccone',                   # Type in your name
+  author_email = 'antonio@ariseo.sk',      # Type in your E-Mail
+  url = 'https://github.com/neisor/caffeination',   # Provide either the link to your github or to your website
+  keywords = ['caffeination', 'caffe', 'caffeine', 'sleep preventer'],   # Keywords that define your package best
+  install_requires=[
+          'pyautogui'
+      ],
+  packages=setuptools.find_packages(),
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+  python_requires='>=3.6'
+)
